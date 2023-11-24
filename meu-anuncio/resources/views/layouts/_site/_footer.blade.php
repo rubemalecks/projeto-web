f179c9e13232358b1c6e06a52e06447413142ad7
Rubem Álecks <rubemalecks@gmail.com>
2023-11-24 17:59:03 -0300

    footer, sobre ...
    Co-Authored-By: Leandro Paulino <117759006+LeandroPaul@users.noreply.github.com>

--- meu-anuncio/resources/views/layouts/_site/_footer.blade.php
+++ meu-anuncio/resources/views/layouts/_site/_footer.blade.php
@@ -1,26 +1,23 @@
 <footer class="page-footer black">
     <div class="container">
         <div class="row">
-            <div class="col l6 s12 ">
+            <div class="col l6 s12">
                 <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.
-                </p>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
             </div>
             <div class="col l4 offset-l2 s12">
                 <h5 class="white-text">Links</h5>
                 <ul>
-                    <li><a class="grey-text text-lighten-3" href="#!">Início</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Sobre</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Contato</a></li>
-
-
+                    <li><a class="grey-text text-lighten-3" href="{{ route('site.home') }}">Início</a></li>
+                    <li><a class="grey-text text-lighten-3" href="{{ route('site.sobre') }}">Sobre</a></li>
+                    <li><a class="grey-text text-lighten-3" href="{{ route('site.contato') }}">Contato</a></li>
                 </ul>
             </div>
         </div>
     </div>
     <div class="footer-copyright">
         <div class="container">
-            © 2023 Copyright Text
+            © 2021 Copyright Text
             <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
         </div>
     </div>
--- meu-anuncio/resources/views/site/contato.blade.php
+++ meu-anuncio/resources/views/site/contato.blade.php
@@ -2,6 +2,31 @@
 
 @section('content')
 <div class="container">
-    <h3 class="center">Contato</h3>
+    <div class="row section">
+        <h3 class="center">Contato</h3>
+        <div class="divider"></div>
+    </div>
+    <div class="row section">
+        <div class="col s12 m7">
+            <img src="{{ asset('img/mapa.jpg') }}" alt="" class="responsive-img">
+        </div>
+        <div class="col s12 m5">
+            <form action="" class="col s12">
+                <div class="input-field">
+                    <input type="text" id="nome" class="validate">
+                    <label for="name">Nome</label>
+                </div>
+                <div class="input-field">
+                    <input type="email" id="email" class="validate">
+                    <label for="email">E-mail</label>
+                </div>
+                <div class="input-field">
+                    <textarea id="mensagem" class="materialize-textarea"></textarea>
+                    <label for="mensagem">Mensagem</label>
+                </div>
+                <button class="btn grey">Enviar</button>
+            </form>
+        </div>
+    </div>
 </div>
 @endsection
