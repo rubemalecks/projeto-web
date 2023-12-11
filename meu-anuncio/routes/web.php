d8d6ad59c90e5eb02c9be366f7766bb28b415ac5
Rubem Álecks <rubemalecks@gmail.com>
2023-12-11 12:47:14 -0300

    web e db

--- meu-anuncio/routes/web.php
+++ meu-anuncio/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 use App\Http\Controllers\Admin\CategoriaController;
 use App\Http\Controllers\Admin\MunicipioController;
 use App\Http\Controllers\Admin\UsuarioController;
@@ -33,13 +32,6 @@ Route::get('/sobre', [PaginaSite::class, 'sobre'])->name('site.sobre');
 Route::get('/contato', [PaginaSite::class, 'contato'])->name('site.contato');
 Route::post('/contato', [PaginaSite::class, 'enviarContato'])->name('site.contato');
 
-/*Route::get('/contato', [
-    'as' => 'site.contato',
-    function () {
-        return view('site.contato');
-    }
-]);*/
-
 Route::get('/anuncio/{id}/{titulo?}', [
     'as' => 'site.anuncio',
     function () {
@@ -48,7 +40,6 @@ Route::get('/anuncio/{id}/{titulo?}', [
 ]);
 
 //Auth::routes();
-
 
 Route::get('/admin/login', ['as' => 'admin.login', function () {
     return view('admin.login.index');
@@ -64,47 +55,30 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/admin/logout', [UsuarioController::class, 'logout'])->name('admin.logout');
 
     Route::get('/admin/usuarios', [UsuarioController::class, 'index'])->name('admin.usuarios');
-
     Route::get('/admin/usuarios/cadastrar', [UsuarioController::class, 'cadastrar'])->name('admin.usuarios.cadastrar');
-
     Route::post('/admin/usuarios', [UsuarioController::class, 'salvar'])->name('admin.usuarios');
-
     Route::get('/admin/usuarios/alterar/{id}', [UsuarioController::class, 'alterar'])->name('admin.usuarios.alterar');
-
     Route::put('/admin/usuarios/atualizar/{id}', [UsuarioController::class, 'atualizar'])->name('admin.usuarios.atualizar');
-
     Route::delete('/admin/usuarios/remover/{id}', [UsuarioController::class, 'remover'])->name('admin.usuarios.remover');
 
 
     Route::get('/admin/paginas', [PaginaAdmin::class, 'index'])->name('admin.paginas');
-
     Route::get('/admin/paginas/alterar/{id}', [PaginaAdmin::class, 'alterar'])->name('admin.paginas.alterar');
-
     Route::put('/admin/paginas/atualizar/{id}', [PaginaAdmin::class, 'atualizar'])->name('admin.paginas.atualizar');
 
     Route::get('/admin/categorias', [CategoriaController::class, 'index'])->name('admin.categorias');
-
     Route::get('/admin/categorias/cadastrar', [CategoriaController::class, 'cadastrar'])->name('admin.categorias.cadastrar');
-
     Route::post('/admin/categorias', [CategoriaController::class, 'salvar'])->name('admin.categorias');
-
     Route::get('/admin/categorias/alterar/{id}', [CategoriaController::class, 'alterar'])->name('admin.categorias.alterar');
-
-    Route::put('/admin/usuarios/atualizar/{id}', [CategoriaController::class, 'atualizar'])->name('admin.categorias.atualizar');
-
-    Route::delete('/admin/usuarios/remover/{id}', [CategoriaController::class, 'remover'])->name('admin.categorias.remover');
+    Route::put('/admin/categorias/atualizar/{id}', [CategoriaController::class, 'atualizar'])->name('admin.categorias.atualizar');
+    Route::delete('/admin/categorias/remover/{id}', [CategoriaController::class, 'remover'])->name('admin.categorias.remover');
 
 
     Route::get('/admin/municipios', [MunicipioController::class, 'index'])->name('admin.municipios');
-
     Route::get('/admin/municipios/cadastrar', [MunicipioController::class, 'cadastrar'])->name('admin.municipios.cadastrar');
-
     Route::post('/admin/municipios', [MunicipioController::class, 'salvar'])->name('admin.municipios');
-
     Route::get('/admin/municipios/alterar/{id}', [MunicipioController::class, 'alterar'])->name('admin.municipios.alterar');
-
     Route::put('/admin/usuarios/atualizar/{id}', [MunicipioController::class, 'atualizar'])->name('admin.municipios.atualizar');
-
     Route::delete('/admin/usuarios/remover/{id}', [MunicipioController::class, 'remover'])->name('admin.municipios.remover');
 
 });
