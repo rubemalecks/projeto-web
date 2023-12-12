fe68ba8d909a91729c27640b1d1acf0a6ece752e
Rubem Álecks <rubemalecks@gmail.com>
2023-12-12 11:36:04 -0300

    48/51
    Co-Authored-By: Leandro Paulino <117759006+LeandroPaul@users.noreply.github.com>

---
+++ meu-anuncio/config/seo.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'titulo' => 'MeuAnuncio.com',
+    'descricao' => 'Site do Projeto de WEB2',
+    'imagem' => 'URL da imagem',
+];
--- meu-anuncio/resources/views/layouts/site.blade.php
+++ meu-anuncio/resources/views/layouts/site.blade.php
@@ -8,46 +8,52 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ isset($seo['titulo']) ? $seo['titulo'] : config('seo.titulo') }}</title>
+    <meta name="description" content="{{ isset($seo['descricao']) ? $seo['descricao'] : config('seo.descricao') }}">
+
+    <!-- Twitter Card -->
+    <meta name="twitter:card" content="summary" />
+
+    <!-- Open Graph -->
+    <meta property="og:url" content="{{ isset($seo['url']) ? $seo['url'] : config('app.url') }}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="{{ isset($seo['titulo']) ? $seo['titulo'] : config('seo.titulo') }}" />
+    <meta property="og:description" content="{{ isset($seo['titulo']) ? $seo['descricao'] : config('seo.descricao') }}" />
+    <meta property="og:image" content="{{ isset($seo['imagem']) ? $seo['imagem'] : config('seo.imagem') }}" />
+
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
     <header>
         @include('layouts._site._nav')
-
     </header>
     <main>
         @if(Session::has('mensagem'))
         <div class="container">
             <div class="row">
-                <div class="card {{Session::get('mensagem')['class']}}">
+                <div class="card {{ Session::get('mensagem')['class'] }}">
                     <div class="card-content">
-                        {{Session::get('mensagem')['msg']}}
+                        {{ Session::get('mensagem')['msg'] }}
                     </div>
                 </div>
             </div>
         </div>
         @endif
-
         @yield('content')
     </main>
     <footer>
         @include('layouts._site._footer')
     </footer>
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-
-    <script src="{{ asset('js/init.js')}}"></script>
-
+    <script src="{{ asset('js/init.js') }}"></script>
 </body>
 
 </html>
